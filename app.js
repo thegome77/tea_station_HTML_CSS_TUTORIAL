bf35bacd8cd946ba0dd05654998fc3f87db7d691
Christian Gomez <thegome77@gmail.com>
2022-11-17 12:15:45 +1100

    initial commit for tea station HTML and CSS tutorial project

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 //setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear());
 // setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
-// show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
 // close nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav');
+});
+// show nav
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav');
 });
